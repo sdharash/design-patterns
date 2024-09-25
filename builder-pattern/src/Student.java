80a644974066e9af96ce5fd818d65d41e69e3cde
sdharash <shubhamdharashivkar8@gmail.com>
2024-09-25 12:38:32 +0530

    Implementation of builder design pattern

--- builder-pattern/src/Student.java
+++ builder-pattern/src/Student.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Student {
+package PACKAGE_NAME;
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int rollNumber;
+    private int age;
+    private String gender;
+    private String email;
+
+    Student(StudentBuilder studentBuilder) {
+        this.firstName = studentBuilder.firstName;
+        this.lastName = studentBuilder.lastName;
+        this.rollNumber = studentBuilder.rollNumber;
+        this.age = studentBuilder.age;
+        this.gender = studentBuilder.gender;
+        this.email = studentBuilder.email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student details : "+ this.firstName + " "+this.lastName;
+    }
 }
